bcf9939f3bb78771e45ef1bdf536f11e7ef97893
natanon <natanon.jari@bumail.net>
2024-03-24 14:22:49 +0700

    create new obstacle

--- Assets/Scripts/Moveplayer.cs
+++ Assets/Scripts/Moveplayer.cs
@@ -1,23 +1,61 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Moveplayer : MonoBehaviour
 {
+    private Rigidbody body;
+    [SerializeField] private float force, mass, acceleration;
+
+    private bool isHolding;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody>();
+        mass = GetComponent<Rigidbody>().mass;
     }
 
     // Update is called once per frame
     void Update()
     {
-       GetComponent<Rigidbody>().AddTorque(0,10,0);
-       if (Input.GetMouseButtonDown(0))
-       {
-           GetComponent<Rigidbody>().AddTorque(0,0,0);
-           GetComponent<Rigidbody>().AddForce(0,0,100);
-       }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isHolding = true;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+           
+            CalculateForce();
+            // Reset the num
+            acceleration = 0f;
+            force = 0f;
+            isHolding = false;
+        }
+
+        // Increment the num
+        if (isHolding)
+        {
+            acceleration += 1f;
+        }
+    }
+    
+    void CalculateForce()
+    {
+        force = mass * acceleration;
+        GetComponent<Rigidbody>().AddForce(0,force,force);
+        isHolding = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("DeathFloor"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        
     }
 }
